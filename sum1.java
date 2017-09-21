9543945b987308da73d3ec543c1623009657ec6c
Jesper Larsson <jesl@avadeaux.net>
2017-09-21 20:32:08 +0200

    deleted unintentionally committed

--- sum1.java
+++
@@ -1,9 +0,0 @@
-public class sum1 {
-    public static void main(String[] args) {
-        int sum = 0, i;
-    for (i = 5; i <= 10; i++) {
-        sum = sum + 2*i;
-    }
-        System.out.println(sum);
-    }
-}
